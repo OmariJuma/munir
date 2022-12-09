d46ea294c7bfdbb27092b86bd00e714293a037b7
Omar Juma <jumao2669@gmail.com>
2022-12-09 23:37:01 +0300

    tweaks on the nav and services cards

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,30 +2,58 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { FaShoppingCart} from "react-icons/fa";
-import './NavBar.css';
+import { FaShoppingCart } from "react-icons/fa";
+import "./NavBar.css";
 const NavBar = () => {
+  const navbar = document.querySelector('.na');
+window.onscroll = () => {
+    if (window.scrollY > 300) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
   return (
-    <Navbar  expand="lg" className="sticky-top na linkie" >
-    <Container>
-      <Navbar.Brand href="#home" className="linkie">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home" className="linkie">Home</Nav.Link>
-          <Nav.Link href="#home" className="linkie">Prodcuts</Nav.Link>
-          {/* <NavDropdown title={<p classsName="linkie">Hi</p>} id="basic-nav-dropdown" bg='success'> */}
+    <Navbar
+      expand="lg"
+      className="sticky-top na linkie"
+      
+    >
+      <Container>
+        <Navbar.Brand href="#home" className="linkie">
+          React-Bootstrap
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="justify-content-center"
+        >
+          <Nav className="mr-auto">
+            <Nav.Link href="#home" className="linkie">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#home" className="linkie">
+              Prodcuts
+            </Nav.Link>
+            {/* <NavDropdown title={<p classsName="linkie">Hi</p>} id="basic-nav-dropdown" bg='success'> */}
             {/* <NavDropdown.Item href="#action/3.2" > */}
-              {/* Rims */}
+            {/* Rims */}
             {/* </NavDropdown.Item> */}
             {/* <NavDropdown.Item href="#action/3.3" >Tyres</NavDropdown.Item> */}
-          {/* </NavDropdown> */}
-          <Nav.Link href="#link" className="linkie">About us</Nav.Link>
-          <Nav.Link href="#link" className="linkie">location</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-    <span className="cartIcon top right"><FaShoppingCart /></span>
-  </Navbar>  );
+            {/* </NavDropdown> */}
+            <Nav.Link href="#link" className="linkie">
+              About us
+            </Nav.Link>
+            <Nav.Link href="#link" className="linkie">
+              location
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+      <span className="cartIcon top right">
+        <FaShoppingCart />
+      </span>
+    </Navbar>
+  );
 };
 export default NavBar;
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -2,12 +2,13 @@ import './services.css';
 import Spare from '../assets/images/spare.png'
 import Card from "react-bootstrap/Card";
 const Services = () => {
-    return (<div className="container-fluid bg ">
+    return (<div className="container-fluid bg " id='container'>
         <h1 className='text-center'>Services We Provide</h1>
 <Card style={{ width: '300px'}} className="md-6 xl-6" id="no1" >
+<Card.Title className='text-center'>Card Title</Card.Title>
       <Card.Img  src={Spare}/>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
@@ -15,19 +16,22 @@ const Services = () => {
       </Card.Body>
     </Card>
     <Card style={{ width: '18rem'}} className="md-6 xl-6" id='no2'>
+    <Card.Title className='text-center bb'>Card Title</Card.Title>
       <Card.Img  src={Spare}/>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
       </Card.Body>
     </Card>
+    
     <Card style={{ width: '18rem'}} className="md-6 xl-6" id='no3'>
+    <Card.Title className='text-center'>Card Title</Card.Title>
       <Card.Img  src={Spare}/>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
