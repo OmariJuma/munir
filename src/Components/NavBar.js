1fc6e3341d9154ca0671f80f782eb4514a08bd7c
Omar Juma <jumao2669@gmail.com>
2023-01-18 01:42:52 +0300

    navbar is good

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,60 +1,70 @@
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
-import { FaShoppingCart } from "react-icons/fa";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { FaRegUserCircle, FaSearch } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 import "../App.css";
 import "./NavBar.css";
 const NavBar = () => {
-//   const navbar = document.querySelector('.na');
-// window.onscroll = () => {
-//     if (window.scrollY > 300) {
-//       navbar.classList.toggle();
-//         navbar.classList.add('nav-active');
-//     } else {
-//         navbar.classList.remove('nav-active');
-//     }
-// };
   return (
     <Navbar
       expand="lg"
-      className="navbar bg-primary navbar-expand-sm navbar-right sticky-top "
-      
+      className="navbar navbar-expand-sm navbar-right sticky-top "
     >
       <Container>
-        <Navbar.Brand href="#home" className="linkie bt-2" >
-          React-Bootstrap
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span>
+            <GiHamburgerMenu />
+          </span>
+        </Navbar.Toggle>
+
+        <Navbar.Brand href="#home" className="linkie bt-2">
+          Muneer Automotive
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <span className="cartIcon">
+          <FaRegUserCircle />
+        </span>
         <Navbar.Collapse
           id="basic-navbar-nav"
           className="justify-content-center"
         >
-          <Nav className="mr-auto">
-            <Nav.Link href="#home" className="linkie">
-              Home
-            </Nav.Link>
-            <Nav.Link href="#home" className="linkie">
-              Products
-            </Nav.Link>
-            {/* <NavDropdown title={<p classsName="linkie">Hi</p>} id="basic-nav-dropdown" bg='success'> */}
-            {/* <NavDropdown.Item href="#action/3.2" > */}
-            {/* Rims */}
-            {/* </NavDropdown.Item> */}
-            {/* <NavDropdown.Item href="#action/3.3" >Tyres</NavDropdown.Item> */}
-            {/* </NavDropdown> */}
-            <Nav.Link href="#link" className="linkie">
-              About us
-            </Nav.Link>
-            <Nav.Link href="#link" className="linkie">
-              location
-            </Nav.Link>
+          <Nav
+            variant="tabs"
+            className="justify-content-center"
+            activeKey="/home"
+          >
+            <Nav.Item>
+              <div id="searchCont">
+                <input
+                  type="search"
+                  id="search"
+                  placeholder="am looking for..."
+                  autoComplete="off"
+                />
+                <Button className="sBtn" type="submit" variant="primary">
+                  <FaSearch/>
+                </Button>
+              </div>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/home">Home</Nav.Link>
+            </Nav.Item>
+            <NavDropdown title="Products" id="nav-dropdown">
+              <NavDropdown.Item eventKey="4.1">Rims</NavDropdown.Item>
+              <NavDropdown.Item eventKey="4.2">Tyres</NavDropdown.Item>
+              <NavDropdown.Item eventKey="4.3">Rims and Tyres</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Item>
+              <Nav.Link eventKey="link-2">Appointment</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-2">About us</Nav.Link>
+            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
-      <span className="cartIcon top right">
-        <FaShoppingCart />
-      </span>
     </Navbar>
   );
 };
