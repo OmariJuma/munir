6199934c6bca1633b83eacd243c5cad4c5022a62
Omar Juma <jumao2669@gmail.com>
2023-02-10 09:24:29 +0300

    form has been added awaiting validation logic

--- src/Components/Pages/BookingPage.js
+++ src/Components/Pages/BookingPage.js
@@ -1,43 +1,146 @@
 import styles from "./BookingPage.module.css";
 import Booked from "./../../assets/images/animations/booked.mp4";
+import Parking from "./../../assets/images/parking.jpg";
+import Parking1 from "./../../assets/images/parking1.jpg";
+import date from "./../../assets/images/date.png";
 import calend from "./../../assets/images/icons/calend.png";
-import { Form,Button } from "react-bootstrap";
+import { Form, Button, Carousel, Col,Row } from "react-bootstrap";
 // import { Row, Col } from "react-bootstrap";
 const BookingPage = () => {
+  var currentDate=new Date();
+  var dd=String(currentDate.getDate()).padStart(2,'0');
+  var mm=String(currentDate.getMonth()+1).padStart(2,'0');
+  var yyyy=String(currentDate.getFullYear());
+currentDate=mm+'/'+dd+'/'+yyyy;
+console.log(currentDate);
+const onClickHandler=(e)=>{
+e.preventDefault()
+}
   return (
-    <div className={styles.container}>
-      <div className={styles.titleHolder}>
-        <span>
-          <img src={calend} alt="A calender icon"/>
-        </span>
-        <h1>Schedule An Appointment</h1>
-      </div>
+    <>
+      <Carousel>
+        <Carousel.Item>
+          <img src={date} alt="parking slot" className={styles.img} />
+          <Carousel.Caption style={{ color: "black" }}>
+            <h3 style={{ color: "white" }}>
+              Schedule a <span style={{ color: "black" }}>Booking Today</span>
+            </h3>
+            Are you seeking professional advice? Or want to book a service slot?
+            Look no further, book a date and time on the form below
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={Parking} alt="parking slot" className={styles.img} />
 
-      <video src={Booked} autoPlay loop muted id={styles.bookanime}></video>
-      <p id={styles.p}>Are you seeking professional advice? Or want to book a service slot? Look no further, book a date and time </p>
-      <p id={styles.p1}>Are you seeking professional advice? Or want to book a service slot? Look no further, book a date and time </p>
-      <Form>
-        <h3>Schedule A Booking</h3>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+          <Carousel.Caption>
+            <h3>Guaranteed Parking slot</h3>
+            We provide free parking slots to our clients, come today and
+            experience our services{" "}
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+      {/* </div> */}
+      <Form className={styles.frmBooking} role="form">
+        <div className={styles.titleHolder}>
+          <span>
+            <img src={calend} alt="A calender icon" />
+          </span>
+          <h1>Schedule An Appointment</h1>
+        </div>
+        <Row>
+          <Col md={6} lg={6} xxl={6}>
+          <Form.Group className="mb-3" controlId="formBasicInput">
+          <Form.Label>Enter First name</Form.Label>
+          <Form.Control type="text" placeholder="John" />
+        </Form.Group>
+
+          </Col>
+          <Col md={6} lg={6} xxl={6}>
+          <Form.Group className="mb-3" controlId="formBasicInput">
+          <Form.Label>Enter Second Name</Form.Label>
+          <Form.Control type="text" placeholder="Doe" />
+        </Form.Group>
+
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={6} lg={6} xxl={6}>
+          <Form.Group className="mb-3" controlId="formBasicInput">
+          <Form.Label>Enter Your phone number</Form.Label>
+          <Form.Control type="number" placeholder="0712345678" />
+        </Form.Group>
+
+          </Col>
+          <Col md={6} lg={6} xxl={6}>
+          <Form.Group className="mb-3" controlId="formBasicInput">
+          <Form.Label>Enter Your Email</Form.Label>
+          <Form.Control type="email" placeholder="example@gmail.com" />
+        </Form.Group>
+
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6} lg={6} xxl={6}>
+          <Form.Group className="mb-3" controlId="formBasicInput">
+          <Form.Label>Vehicle Make</Form.Label>
+          <Form.Control type="text" placeholder="e.g Toyota"/>
+        </Form.Group>
+
+          </Col>
+          <Col md={6} lg={6} xxl={6}>
+          <Form.Group className="mb-3" controlId="formBasicInput">
+          <Form.Label>Vehicle Model</Form.Label>
+          <Form.Control type="text" placeholder="e.g Aauris"/>
+        </Form.Group>
+
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6} lg={6} xxl={6}>
+          <Form.Group className="mb-3" controlId="formBasicInput">
+          <Form.Label>Registration Number</Form.Label>
+          <Form.Control type="text" placeholder="e.g KCA 123A"/>
+        </Form.Group>
+
+          </Col>
+          <Col md={6} lg={6} xxl={6}>
+          <Form.Group className="mb-3" controlId="formBasicInput">
+          <Form.Label>Pick a date</Form.Label>
+          <Form.Control type="date" placeholder="(mm/dd/yy)"
+       min={currentDate} />
+        </Form.Group>
+
+          </Col>
+        </Row>
+
+
+        <Button variant="primary" type="submit" onClick={onClickHandler}>
+          Submit
+        </Button>
       </Form>
-    </div>
+      <h5>If you already have a booking <a>click here</a></h5>
+      <h2>Safety of your data <span id={styles.safety}>is our top priority</span></h2>
+      <Row>
+        <Col xs={12} sm={6} md={6} lg={6}>
+          
+          </Col> 
+        <Col xs={12} sm={6} md={6} lg={6}>
+          
+          </Col> 
+      </Row>
+      <h2><span id={styles.safety1}>Guaranteed Booking</span> with our qualified technicians</h2>
+
+      <Row>
+        <Col xs={12} sm={6} md={6} lg={6}>
+          
+          </Col> 
+        <Col xs={12} sm={6} md={6} lg={6}>
+          
+          </Col> 
+      </Row>
+    </>
   );
 };
 
