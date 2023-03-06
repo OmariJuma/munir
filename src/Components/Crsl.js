38522e10a48b3a465366deb6a929bd1a99d81360
Omar Juma <jumao2669@gmail.com>
2023-03-06 15:24:15 +0300

    carousel on the home section added

--- src/Components/Crsl.js
+++ src/Components/Crsl.js
@@ -1,20 +1,44 @@
-import styles from"./Carousel.module.css";
+import styles from "./Carousel.module.css";
 import bugatti from "./../assets/images/buggati.webp";
 import bugatti2 from "./../assets/images/hh.webp";
 import MiniCard from "./UI/MiniCard";
 import Input from "./UI/Input";
+import { Carousel } from "react-bootstrap";
 const Crsl = () => {
   return (
-    <div className={styles.contain}>
-      <img src={bugatti} alt="the home screen" id={styles.def} />
-      <img src={bugatti2} alt="the home screen" id={styles.sub} />
-      <h1 style={{  position: "absolute",  top: "2rem"
-}}>Muneer Automotive, The home of Tires & Rims</h1>
-      <MiniCard/>
-      <div className={styles.mini}> 
-      <Input/>
-      </div>
-    </div>
+    <Carousel>
+      <Carousel.Item>
+        <img src={bugatti} alt="the home screen" id={styles.def} />
+        <img src={bugatti2} alt="the home screen" id={styles.sub} />
+        <h1 style={{ color: "white", position: "absolute", top: "2rem",textAlign:"center",right:"0",left:"0" }} id={styles.crslH1}>
+          Muneer Automotive, The home of Tires & Rims
+        </h1>
+        <Carousel.Caption>
+          <MiniCard />
+          <div className={styles.mini}>
+            <Input />
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={bugatti} alt="the home screen" id={styles.def} />
+        <img src={bugatti2} alt="the home screen" id={styles.sub} />
+        <Carousel.Caption>
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={bugatti} alt="the home screen" id={styles.def} />
+        <img src={bugatti2} alt="the home screen" id={styles.sub} />
+        <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
 export default Crsl;
--- src/Components/UI/MiniCard.js
+++ src/Components/UI/MiniCard.js
@@ -1,29 +1,34 @@
-import { FaCalendarAlt,FaPhone } from "react-icons/fa";
+import { FaCalendarAlt, FaPhone } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import wl from "./../../assets/images/goodyear1.jpeg";
 import styles from "./Minicard.module.css";
+
 const MiniCard = () => {
   return (
-    <>
+    <div className={styles.miniCardContainer}>
       <div className={styles.mini}>
-        <img src={wl} alt="wheel" />
-        <p className={styles.p}>BuyTires and Rims</p>
+        <Link to="/products">
+          <img src={wl} alt="wheel" />
+          <p className={styles.p}>BuyTires and Rims</p>
+        </Link>
+      </div>
+      <div className={styles.mini}>
+        <Link to="/booking">
+          <span>
+            <FaCalendarAlt className={styles.ic} />
+            <p className={styles.p2}>Book an appointment</p>
+          </span>
+        </Link>
       </div>
       <div className={styles.mini}>
         <span>
-          <FaCalendarAlt className={styles.ic}/>
-          <p className={styles.p2}>Book an appointment</p>
-
+          <a href="tel:+254707686784">
+            <FaPhone className={styles.ic1} />
+            <p className={styles.p3}>Call us</p>
+          </a>
         </span>
       </div>
-      <div className={styles.mini}>
-        <span>
-          <FaPhone className={styles.ic1} href='google'/>
-          <p className={styles.p3}>Call us</p>
-
-        </span>
-      </div>
-
-    </>
+    </div>
   );
 };
 
