026c67ed2d74a3e110529aebab288b6726eda97c
Omar Juma <jumao2669@gmail.com>
2023-03-26 16:09:48 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import CartProvider from "./Components/store/CartProvider";
 import Cart from "./Components/UI/Cart";
 import Footer from "./Components/UI/Footer";
 import SidebarIcons from "./Components/UI/SidebarIcons";
+import Spinner from "./Components/UI/Spinner";
 // import BreadCrumb from "./Components/UI/BreadCrumb";
 
-
 function App() {
-  const [cartIsOpen,setCartIsOpen]=useState(false);
+  const [cartIsOpen, setCartIsOpen] = useState(false);
 
-const showCart=()=>{
-  setCartIsOpen(true)
-}
-const hideCart=()=>{
-  setCartIsOpen(false)
-}
+  const showCart = () => {
+    setCartIsOpen(true);
+  };
+  const hideCart = () => {
+    setCartIsOpen(false);
+  };
 
-  return (<CartProvider>
-      <NavBar onShowCart={showCart}/>
-      {/* <BreadCrumb/> */}
-      <SidebarIcons/>
+  return (
+    <CartProvider>
+      <Suspense fallback={<Spinner />}>
+        <NavBar onShowCart={showCart} />
+        {/* <BreadCrumb/> */}
+        <SidebarIcons />
 
-    <Footer/>
-    {cartIsOpen&&<Cart onHideCart={hideCart}/>}
+        <Footer />
+        {cartIsOpen && <Cart onHideCart={hideCart} />}
+      </Suspense>
     </CartProvider>
   );
 }
--- src/Components/Pages/Error404.js
+++ src/Components/Pages/Error404.js
@@ -1,14 +1,15 @@
 import { useRouteError } from "react-router";
 import { Link } from "react-router-dom";
 import styles from "./Error404.module.css";
+import e404 from "./../../assets/images/logos/Error404.png";
 const Error404 = () => {
 // if (status === 404) {
 //         throw new Response(<h2 ><br/>404Not Found<br/></h2>);
 //       }
   
     return (<div id={styles.errorpage}>
-<h1>Ooops!</h1>
-<p>Sorry, an unexpected error has occured</p>
+<img style={{height:"90%"}} src={e404} alt="error 404"/>
+<p>The page you are looking for does not exist</p>
 <Link to="/" >homepage</Link>
 <Link to="products/rims" >rims</Link>
 <Link to="/products/tyres" >tyres</Link>
