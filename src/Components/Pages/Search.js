7ed0d505607d8cb6105c62f19da0aff52569caba
Omar Juma <jumao2669@gmail.com>
2023-05-13 22:44:47 +0300

    added loading, and failed states

--- src/Components/Pages/Search.js
+++ src/Components/Pages/Search.js
@@ -3,20 +3,24 @@ import Caard from "../Caard";
 import { Col, Row } from "react-bootstrap";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { v4 } from "uuid";
+import Spinner from "../UI/Spinner";
+import NoInternet from "../UI/NoInternet";
 
 const Search = (props) => {
-  // const location = useLocation();
-  // const state = location.state;
-  // console.log(state);
   const { key } = useParams();
   const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [failed, setFailed] = useState(false);
 
   const getRecords = async () => {
+    setIsLoading(true)
     await axios
       .get(
         `https://test.muneerautomotive.co.ke/api/products/someProducts/${key}`
       )
       .then((res) => {
+        setIsLoading(false)
         if (res.data.length !== 0) {
           setSearchResults(res.data);
           console.log(res.data);
@@ -25,35 +29,42 @@ const Search = (props) => {
         }
       })
       .catch((err) => {
+        setIsLoading(false)
+        setFailed(true)
         console.log(err);
       });
   };
   useEffect(() => {
     getRecords();
   }, [key]);
-    return (
+  return (
     <div className="container">
       <p>Search Results</p>
-      <Row>
-        {searchResults.map((state) => (
-          <Col xs={5} md={3} lg={3} xxl={3}>
-            <div style={{ marginTop: "3rem" }}>
-              <Caard
-                id={state.id}
-                price={state.price}
-                title={state.name}
-                description={state.description}
-                offer={Math.floor(1.1 * state.price)}
-                image={state.image}
-                brand={state.brand}
-                material={state.material}
-                units={state.units}
-                product={true}
-              />
-            </div>
-          </Col>
-         ))}
-      </Row>
+      {isLoading && <Spinner />}
+      {failed && <NoInternet />}
+
+      {!isLoading&&<Row>
+          {searchResults.map((state) => (
+            <Col xs={5} md={3} lg={3} xxl={3}>
+              <div style={{ marginTop: "3rem" }}>
+                <Caard
+                  key={v4()}
+                  id={state.id}
+                  price={state.price}
+                  title={state.name}
+                  description={state.description}
+                  offer={Math.floor(1.1 * state.price)}
+                  image={state.image}
+                  brand={state.brand}
+                  material={state.material}
+                  units={state.units}
+                  product={true}
+                />
+              </div>
+            </Col>
+          ))}
+        </Row>
+      }
     </div>
   );
 };
