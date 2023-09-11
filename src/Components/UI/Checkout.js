a7f2bfe5676071c0c1c0c370480c928496098232
Omar Juma <jumao2669@gmail.com>
2023-09-11 11:38:23 +0300

    Feature: I have added the address section but its not perfect yet

--- src/Components/UI/Checkout.js
+++ src/Components/UI/Checkout.js
@@ -5,12 +5,34 @@ import Ceo from "../../assets/images/People/Mr_Mahmoud.jpeg";
 import { useState } from "react";
 import axios from "axios";
 import { lazy } from "react";
+import counties from "../Utilities/counties";
+import constituencies from "../Utilities/Constituencies";
 const Footer = lazy(() => import("./Footer"));
-
 
 const Checkout = () => {
   const [amount, setAmount] = useState(1);
   const [phone, setPhone] = useState(null);
+  const [customerInfo, setCustomerInfo] = useState({
+    firstName: "",
+    secondName: "",
+    phone: "",
+    altPhone: "",
+    email: "",
+    address: "",
+    addInfo: "",
+    county: {
+      countyName: "",
+      countyCode: "",
+    },
+    constituency: "",
+  });
+  const handleInputChange = (event) => {
+    setCustomerInfo({
+      ...customerInfo,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
     axios
@@ -23,39 +45,174 @@ const Checkout = () => {
       })
       .catch((err) => console.log(err));
   };
-  console.log(phone);
+  console.log(customerInfo.county);
   return (
     <>
-      <Row>
-        {/* Left Section */}
-        <Col xs={12} sm={12} md={6} lg={6} className={classes.customCol}>
-          <Image src={Ceo} className={classes.left} alt="Mr. Mrefu" />
-        </Col>
-        {/* Right Section */}
-        <Col xs={12} sm={12} md={6} lg={6} className={classes.customCol}>
-          <div className={classes.checkoutContent}>
-            <h1>Checkout</h1>
-            <p>Please complete your purchase by providing payment details</p>
-            <Image
-              src={LipaNaMpesa}
-              className={classes.mpesa}
-              alt="Pay using mpesa"
-            />
-            <Form className={classes.form} onSub>
-              <Form.Group className="mb-3" controlId="phone">
-                <Form.Label>Enter your mobile number</Form.Label>
+      <div className={classes.checkoutContent}>
+        <h1>Checkout</h1>
+        <p className={classes.disclaimer}>
+          : Dear customer, we only support payments via mpesa, If you want to
+          make purchase using other payment options please contact us on{" "}
+          <a href="tel:+254722174444">+254722174444</a>
+          or
+          <a href="tel:+254707686784">+254707686784</a>
+          for further arrangements. <br />
+          We use third-party courier services such naekana, G4s, etc to deliver
+          your goods. Hence, you will be required to pick up your order at their
+          office.
+        </p>
+        <Image
+          src={LipaNaMpesa}
+          className={classes.mpesa}
+          alt="Pay using mpesa"
+        />
+        <h2>Address</h2>
+        <Form className={classes.form}>
+          <Row>
+            <Col md={6} lg={6} xxl={6}>
+              <Form.Group className="mb-3" controlId="formBasicInput">
+                <Form.Label>Enter First name</Form.Label>
                 <Form.Control
-                  type="number"
-                  onChange={(e) => setPhone(e.target.value)}
+                  type="text"
+                  name="firstName"
+                  placeholder="John"
+                  value={customerInfo.firstName}
+                  onChange={handleInputChange}
+                  minLength={3}
+                  required
                 />
               </Form.Group>
-              <Button type="submit" onClick={submitHandler}>
-                Pay Ksh 1000
-              </Button>
-            </Form>
-          </div>
-        </Col>
-      </Row>
+            </Col>
+            <Col md={6} lg={6} xxl={6}>
+              <Form.Group className="mb-3" controlId="formBasicInput">
+                <Form.Label>Enter Second Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Doe"
+                  onChange={handleInputChange}
+                  name="secondName"
+                  value={customerInfo.secondName}
+                  required
+                  minLength={3}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6} lg={6} xxl={6}>
+              <Form.Group className="mb-3" controlId="formBasicInput">
+                <Form.Label>Mobile number</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="firstName"
+                  placeholder="John"
+                  value={customerInfo.phone}
+                  onChange={handleInputChange}
+                  minLength={3}
+                  required
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6} lg={6} xxl={6}>
+              <Form.Group className="mb-3" controlId="formBasicInput">
+                <Form.Label>Alternative Mobile Number</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Doe"
+                  onChange={handleInputChange}
+                  name="secondName"
+                  value={customerInfo.altPhone}
+                  required
+                  minLength={3}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6} lg={6} xxl={6}>
+              <Form.Group className="mb-3" controlId="formBasicInput">
+                <Form.Label>Area name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="address"
+                  placeholder="eg. Lavington West Phase 2"
+                  value={customerInfo.address}
+                  onChange={handleInputChange}
+                  minLength={3}
+                  required
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6} lg={6} xxl={6}>
+              <Form.Group className="mb-3" controlId="formBasicInput">
+                <Form.Label>Additional Information</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Provide any additional information"
+                  onChange={handleInputChange}
+                  name="addInfo"
+                  value={customerInfo.addInfo}
+                  minLength={3}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6} lg={6} xxl={6}>
+              <Form.Group className="mb-3" controlId="countyAndConstituency">
+                <Form.Label>Select County</Form.Label>
+                <select
+                  className="form-select"
+                  aria-label="Default select example"
+                  name="county"
+                  value={customerInfo.county}
+                  onChange={handleInputChange}
+                >
+                  <option value="">Open this menu</option>
+                  {counties.map((county) => (
+                    <option key={county.code} value={county.county}>
+                      {county.county}
+                    </option>
+                  ))}
+                </select>
+              </Form.Group>
+            </Col>
+            <Col md={6} lg={6} xxl={6}>
+              <Col md={6} lg={6} xxl={6}>
+                <Form.Group className="mb-3" controlId="formBasicInput">
+                  <Form.Label>City/ Constituency/ Location</Form.Label>
+                  <select
+                    className="form-select"
+                    aria-label="Default select example"
+                    name="constituency" // Set the name attribute to "constituency"
+                    value={customerInfo.constituency} // Set the selected value to customerInfo.constituency
+                    onChange={handleInputChange} // Handle the change event
+                  >
+                    <option value="">Open this select menu</option>
+                    {constituencies
+                      .filter(
+                        (constituency) =>
+                          constituency["county-code"] ===
+                          customerInfo.county.countyCode
+                      )
+                      .map((constituency) => (
+                        <option
+                          key={constituency.code}
+                          value={constituency.constituency}
+                        >
+                          {constituency.constituency}
+                        </option>
+                      ))}
+                  </select>{" "}
+                </Form.Group>
+              </Col>
+            </Col>
+          </Row>
+          <Button type="submit" onClick={submitHandler}>
+            Pay Ksh 1000
+          </Button>
+        </Form>
+      </div>
       <Footer />
     </>
   );
